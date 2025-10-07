15a0e2168701d6e4460d303942a09c1bfcd5662d
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2025-10-07 10:58:47 +0100

    added class files on variables, strings and operators

---
+++ WK1-2-variables-strings-operators.js
@@ -0,0 +1,86 @@
+var firstName = 'Tolu';
+console.log(firstName);
+
+firstName = "Ayo";
+console.log(firstName);
+
+let lastName = "Niyi";
+console.log(lastName);
+
+lastName = "Sogo";
+console.log(lastName)
+
+const age = 10;
+console.log(age);
+// you cannot change the value of a constant variable
+// age = 15;
+console.log(age)
+
+var middleName = "Byteme";
+console.log(middleName);
+{
+    var middleName = "Feyikemi";
+    console.log(middleName);
+}
+
+console.log(middleName);
+
+let num1 = 14;
+console.log(num1);
+{
+    let num1 = 20;
+    console.log(num1);   
+}
+
+console.log(num1);
+
+const num2 = 50;
+console.log(num2);
+{
+    const num2 = 17;
+    console.log(num2);
+}
+console.log(num2);
+
+let sentence = 'Ade is going to school';
+let sentence2 = "Ade's boy is going to school";
+
+let sentence3 = '"WHO" stands for World Health Organizatio';
+
+let sentence4 = `Ade's boy is going to school "late"`;
+
+let firstNum = 5;
+let secondNum = 2;
+console.log(firstNum + secondNum);
+console.log(firstNum - secondNum);
+console.log(firstNum * secondNum);
+console.log(firstNum / secondNum);
+console.log(firstNum ** secondNum);
+console.log(firstNum % secondNum);
+
+let a1 = 10;
+console.log(a1);
+a1++;
+console.log(a1);
+++a1;
+console.log(a1);
+
+a1--;
+console.log(a1);
+--a1;
+console.log(a1);
+
+console.log(a1++);
+console.log(a1);
+
+
+console.log(++a1);
+console.log(a1);
+
+
+console.log(a1--);
+console.log(a1);
+
+
+console.log(--a1);
+console.log(a1);
