f20823e4914b61e9889cb14876d7dc2730e0a823
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2025-10-27 11:07:21 +0100

    added class files on for loop

--- WK4-1-for-loop.js
+++ WK4-1-for-loop.js
@@ -48,10 +48,9 @@ for (let i = 0; i < 10; i++) {
     } 
 }
 
-for (let i = 0; i < 10; i++) {
-    console.log(i);  
-    
+for (let i = 0; i < 10; i++) {    
     if (i === 5) {
         break;
     } 
+    console.log(i);  
 }
